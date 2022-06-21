194439f6931638efd35ac43514617cf77cf59cc8
enesgokhanozcan <2016010213018@ogrenci.karabuk.edu.tr>
2022-06-21 16:34:21 +0300

    Some model and repository created

--- Hasscript/src/main/java/com/example/Hasscript/model/User.java
+++ Hasscript/src/main/java/com/example/Hasscript/model/User.java
@@ -8,30 +8,85 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-    @Column(name = "name")
-    private String  name;
-    @Column(name = "surname")
+    private Long Id;
+    @Column(name="identityNumber")
+    private Long identityNumber;
+    @Column(name="name")
+    private String name;
+    @Column(name="surname")
     private String surname;
+    @Column(name="email")
+    private String email;
+    @Column(name="password")
+    private String password;
+    @Column(name="phoneNumber")
+    private String phoneNumber;
+    @Column(name="bloodType")
+    private String bloodType;
+
 
     public User() {
     }
-    public User(String name, String surname) {
+    public User(Long  identityNumber,String name, String surname,String email,String password,String phoneNumber) {
+        this.identityNumber=identityNumber;
         this.name = name;
         this.surname = surname;
+        this.email=email;
+        this.password=password;
+        this.phoneNumber=phoneNumber;
     }
 
+    public Long getIdentityNumber() {
+        return identityNumber;
+    }
+
+    public void setIdentityNumber(Long identityNumber) {
+        this.identityNumber = identityNumber;
+    }
     public String getName(){
-        return name;
+        return  name;
     }
-    public void setName(String name){
-        this.name = name;
+    public void setName(String Name){
+        this.name=name;
     }
 
-    public String getSurname(){
+    public String getSurname() {
         return surname;
     }
-    public void setSurname(String surname){
+
+    public void setSurname(String surname) {
         this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getBloodType() {
+        return bloodType;
+    }
+
+    public void setBloodType(String bloodType) {
+        this.bloodType = bloodType;
     }
 }
--- Hasscript/src/main/java/com/example/Hasscript/repository/HospitalRepository.java
+++ Hasscript/src/main/java/com/example/Hasscript/repository/HospitalRepository.java
@@ -1,10 +1,9 @@
 package com.example.Hasscript.repository;
-import com.example.Hasscript.model.User;
+
+import com.example.Hasscript.model.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-
+public interface HospitalRepository extends JpaRepository<Hospital,Long> {
 }
